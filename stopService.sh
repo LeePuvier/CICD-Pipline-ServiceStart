01831ba21c50b8c583b176ca11bffc7b6fc141f2
LeePuvier <li_pjun@163.com>
2020-07-02 23:04:59 +0800

    Update StopService.sh

--- stopService.sh
+++ stopService.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pid=`ps -ef | ${1} | grep -v grep | awk '{print $2}'`
+echo 'ps -ef | "${1}" | grep -v grep | awk '{print $2}''
+pid=`ps -ef | "${1}" | grep -v grep | awk '{print $2}'`
 if [[ -n "$pid" ]];then
 #!kill -9 强制终止
 	 echo "kill -9 的pid:" $pid
